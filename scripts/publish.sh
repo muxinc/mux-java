9bc2cb4dd2bbee00197041e38ceaa41751ff3f73
Jared Smith <jsmith@mux.com>
2022-05-05 12:02:30 -0400

    Update publish script
    Signed-off-by: Jared Smith <jsmith@mux.com>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,4 +2,7 @@
 
 echo "ENV VARS"
 echo "========"
-echo "Sonatype username: [${ORG_GRADLE_PROJECT_sonatypePassword}]"
+echo "Sonatype username is ${ORG_GRADLE_PROJECT_sonatypeUsername}"
+echo "Sonatype password is ${#ORG_GRADLE_PROJECT_sonatypePassword} digits long"
+echo "Signing Key is ${#ORG_GRADLE_PROJECT_signingKey} digits long"
+echo "Signing Password is ${#ORG_GRADLE_PROJECT_signingPassword} digits long"
