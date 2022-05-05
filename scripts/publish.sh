c31c92bb1209859f547620bf87fb27b2e33d5e68
Jared Smith <jsmith@mux.com>
2022-05-05 13:19:35 -0400

    Add an md5 checksum just for fun
    Signed-off-by: Jared Smith <jsmith@mux.com>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,9 +2,11 @@
 
 echo "ENV VARS"
 echo "========"
-echo "Sonatype username is ${ORG_GRADLE_PROJECT_sonatypeUsername}"
+echo "Sonatype username is ${#ORG_GRADLE_PROJECT_sonatypeUsername} digits long"
 echo "Sonatype password is ${#ORG_GRADLE_PROJECT_sonatypePassword} digits long"
 echo "Signing Key is ${#ORG_GRADLE_PROJECT_signingKey} digits long"
 echo "Signing Password is ${#ORG_GRADLE_PROJECT_signingPassword} digits long"
+key_sum=$(echo -n "${ORG_GRADLE_PROJECT_signingKey}" | md5sum)
+echo "Signing key checksum is ${key_sum}"
 echo "============"
 echo "END ENV VARS"
